034d73a3cc20c8e752c18ad58ce9304899b1e64f
sebaslh12 <sebaslh12@gmail.com>
2019-05-01 19:22:33 -0500

    Adding create blog link Removing NavBar component Fixing Routing bug

--- imports/ui/containers/AppContainer.jsx
+++ imports/ui/containers/AppContainer.jsx
@@ -1,11 +1,14 @@
+import { Meteor } from 'meteor/meteor'
+import { Link } from 'react-router-dom'
 import React from 'react';
 
 import MainContainer from './MainContainer.jsx';
 
 export default class AppContainer extends React.Component {
-    
+
     state = this.getMeteorData();
-    
+    isAdmin = Roles.userIsInRole(Meteor.userId(), ['admin']);
+
 
     getMeteorData() {
         return { isAuthenticated: Meteor.userId() !== null };
@@ -23,9 +26,47 @@ export default class AppContainer extends React.Component {
         }
     }
 
+    logout(e) {
+        e.preventDefault();
+        Meteor.logout((err) => {
+            if (err) {
+                console.error(err.reason);
+            } else {
+                this.props.history.push('/login');
+            }
+        });
+    }
+
     render() {
+        const user = Meteor.user()
         return (
-            <MainContainer />
+            <div>
+                <nav className="navbar navbar-default navbar-static-top">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">Blogs App</a>
+                        </div>
+                        <div className="navbar-collapse">
+                            {
+                                this.isAdmin ? <ul className="nav navbar-nav navbar-left">
+                                    <li className="nav-item">
+                                        <Link to={'/blogForm'}>Create Blog</Link>
+                                    </li>
+                                </ul> : ''
+                            }
+                            <ul className="nav navbar-nav navbar-right">
+                                <li className="nav-item">
+                                    <a>{user.username}</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a href="#" onClick={this.logout.bind(this)}>Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <MainContainer />
+            </div>
         );
     }
 }
--- imports/ui/containers/MainContainer.jsx
+++ imports/ui/containers/MainContainer.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 import BlogsPage from '../pages/BlogsPage'
-import NavBar from '../NavBar.jsx';
 
 export default class MainContainer extends React.Component {
     render(){
         const user = Meteor.user()
         return (
             <div>
-                <NavBar username={user ? user.username : ''}></NavBar>
                 <BlogsPage></BlogsPage>
             </div>
         )
